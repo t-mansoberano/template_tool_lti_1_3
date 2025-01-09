64b5fdb451e1dafb39ca8ea7df8cba4a6c60983d
Jose Manuel Campos Soberano <t-mansoberano@itesm.mx>
2025-01-09 07:55:12 -0600

    Add evaluation components for instructors, students, and collaborators
    This commit introduces components for evaluations targeted at different user roles: instructors, students, and external collaborators. Each component includes its template, styling, and basic test setup, laying the foundation for specific evaluation functionalities. Data integration and interaction logic for these components have also been initiated.

---
+++ gec.client/src/app/features/external-collaborators/evaluations/evaluations.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-evaluations',
+  standalone: true,
+  imports: [],
+  templateUrl: './evaluations.component.html',
+  styleUrl: './evaluations.component.css'
+})
+export class EvaluationsComponent {
+
+}
---
+++ gec.client/src/app/features/students/evaluations/evaluation.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-evaluation',
+  standalone: true,
+  imports: [],
+  templateUrl: './evaluation.component.html',
+  styleUrl: './evaluation.component.css'
+})
+export class EvaluationComponent {
+
+}
