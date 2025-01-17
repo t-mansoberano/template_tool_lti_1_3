0ee6f498267678f8dd9536d08b7616e1bfdf939f
Jose Manuel Campos Soberano <t-mansoberano@itesm.mx>
2025-01-17 09:07:48 -0600

    Enhance evidence list UI and functionality.
    Updated the component to improve styling, display structure, and interactivity. Added support for different file types with corresponding icons and links. Integrated additional features from the design system for better consistency and modularity.

--- gec.client/src/app/features/instructors/evaluations/components/evidence-list/evidence-list.component.ts
+++ gec.client/src/app/features/instructors/evaluations/components/evidence-list/evidence-list.component.ts
@@ -1,15 +1,27 @@
-import {Component, Input} from '@angular/core';
-import {NgForOf} from '@angular/common';
+import {Component, Input, ViewEncapsulation} from '@angular/core';
+import {NgClass, NgForOf, NgIf} from '@angular/common';
+import {
+  BmbCardComponent,
+  BmbCardContentComponent,
+  BmbCardHeaderComponent,
+  BmbIconComponent
+} from '@ti-tecnologico-de-monterrey-oficial/ds-ng';
 
 @Component({
   selector: 'app-evidence-list',
   standalone: true,
   imports: [
-    NgForOf
+    NgForOf,
+    BmbCardComponent,
+    BmbCardContentComponent,
+    BmbCardHeaderComponent,
+    BmbIconComponent,
+    NgClass,
+    NgIf
   ],
   templateUrl: './evidence-list.component.html',
   styleUrl: './evidence-list.component.css'
 })
 export class EvidenceListComponent {
-  @Input() evidences!: { id: string; name: string; feedback: string; grade: number; speedGraderLink: string }[];
+  @Input() evidences!: { id: string; name: string; feedback: string; grade: number; speedGraderLink: string; fileLink: string; fileType: string }[];
 }
