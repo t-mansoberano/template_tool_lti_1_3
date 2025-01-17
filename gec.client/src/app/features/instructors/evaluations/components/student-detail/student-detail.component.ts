a62a26c295226bf6e12b9578a020fbd782a1d719
Jose Manuel Campos Soberano <t-mansoberano@itesm.mx>
2025-01-17 00:47:01 -0600

    Add BmbDividerComponent and update student detail layout
    Integrated the BmbDividerComponent to enhance UI separation in the student detail component. Updated the template to display the student ID and rearranged elements for better structure and readability. These changes improve the overall clarity and user experience of the component.

--- gec.client/src/app/features/instructors/evaluations/components/student-detail/student-detail.component.ts
+++ gec.client/src/app/features/instructors/evaluations/components/student-detail/student-detail.component.ts
@@ -1,13 +1,15 @@
 import {Component, Input} from '@angular/core';
 import {StudentEvaluationComponent} from '../student-evaluation/student-evaluation.component';
 import {EvidenceListComponent} from '../evidence-list/evidence-list.component';
+import {BmbDividerComponent} from '@ti-tecnologico-de-monterrey-oficial/ds-ng';
 
 @Component({
   selector: 'app-student-detail',
   standalone: true,
   imports: [
     StudentEvaluationComponent,
-    EvidenceListComponent
+    EvidenceListComponent,
+    BmbDividerComponent
   ],
   templateUrl: './student-detail.component.html',
   styleUrl: './student-detail.component.css'
